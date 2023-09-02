36d1e8baa06fd085f9ba8a97d31dbe6d0d1f987f
marcoweb <marcocarvalho.web@gmail.com>
2023-09-02 14:39:59 +0000

    Conceitos de OO

---
+++ Cliente.cs
@@ -0,0 +1,6 @@
+namespace exercicio_090223 {
+    public class Cliente {
+        public String nome;
+        public double saldo;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿// Crie um algoritmo em C# que, tendo 2 variáveis contendo a base e a altura de um triangulo, calcule sua área. ( b * h / 2 )
+﻿using exercicio_090223;
+
+// Crie um algoritmo em C# que, tendo 2 variáveis contendo a base e a altura de um triangulo, calcule sua área. ( b * h / 2 )
+
 int b = 20; // Base
 int h = 10; // Al
 
@@ -24,4 +27,29 @@ for(int cont = 0;cont < 10;cont++) {
 foreach(int item in resultado) { 
     Console.WriteLine(item);
 }
+
+// Exemplo Procedural
+
+String nomeCliente = "Marco Carvalho";
+double saldoCliente = 546.85;
+
+String nomeCliente2 = "Maria Aparecida";
+double saldoCliente2 = 621.52;
+
+// Exemplo OO
+Cliente cliente1 = new Cliente();
+cliente1.nome = "Marco Carvalho";
+cliente1.saldo = 546.85;
+
+Cliente cliente2 = new Cliente();
+cliente2.nome = "Maria Aparecida";
+cliente2.saldo = 621.52;
+
+Cliente cliente3 = cliente1;
+cliente3.nome = "Fulano da Silva";
+
+Console.WriteLine(cliente1.nome);
+Console.WriteLine(cliente3.nome);
+
+
 
